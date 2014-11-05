fb505e782031358c122465e50317212239bdec28
hjshroyer <hugh-shroyer@uiowa.edu>
2014-11-05 11:11:07 -0600

    Create utility.java

---
+++ src/org/uiowa/cs2820/engine/utility.java
@@ -0,0 +1,29 @@
+package src.org.uiowa.cs2820.engine;
+import java.io.ByteArrayInputStream;
+
+public class utility implements Serializable{
+	public static byte[] convert(Object O) {
+		  // private method converts objects into byte array
+		  ByteArrayOutputStream M = new ByteArrayOutputStream();
+		  ObjectOutput N = null;
+		  try {
+			N = new ObjectOutputStream(M);
+			N.writeObject(O);
+		    } 
+		  catch (IOException e) {
+	        return null; // wrong, but should not happen	    	
+		    }
+		  return M.toByteArray();
+		  }
+		
+		public static Object revert(byte[] seq) {
+		  Object O = null;  // default value
+		  try {
+			ByteArrayInputStream M = new ByteArrayInputStream(seq);
+			ObjectInputStream N = new ObjectInputStream(M);
+			O = N.readObject();
+			}
+		  catch (Exception e) { };
+			return O;
+		  }
+}
