9a75f042a1215bad7fa6ed29461ba40ffdd872c9
hjshroyer <hugh-shroyer@uiowa.edu>
2014-11-05 12:24:52 -0600

    Update keyStorage.java

--- src/org/uiowa/cs2820/engine/keyStorage.java
+++ src/org/uiowa/cs2820/engine/keyStorage.java
@@ -1,22 +1,30 @@
 package src.org.uiowa.cs2820.engine;
 
-import src.org.uiowa.cs2820.engine.diskSpace;
+import src.org.uiowa.cs2820.engine.Diskspace;
 import src.org.uiowa.cs2820.engine.Field;
+import src.org.uiowa.cs2820.engine.Allocate;
 
-public class keystorage {
+public class keyStorage {
 	
 	private Node node;
 	
-	public boolean init(byte [] key){
-	//Determines if Key exists in Disk Space
+	
+	keyStorage(Field f){
+		byte[] myByte = utility.convert(f);
+		ArrayList[] st = valueStorage.load(f.)
+		node = Node(myByte, st);
+	}
+	public boolean init(byte[] key){
 		for (Node n : diskSpace){
-			if (Arrays.equals(n.Key, key)){
+			byte[] bn = utility.revert(n);
+			if(Arrays.equals(key, bn)){
 				return true;
 			}
 			else{
 				return false;
 			}
 		}
+			
 	}
 	public void clear(){
 		
@@ -28,11 +36,13 @@ public class keystorage {
 	}
 	public void put(int area, Node node){
 	//Inserts Node into specified area number of Disk Space	
-		byte[] myByte = Field.convert(node);
-		diskSpace.add(area, myByte);
+		byte[] myByte = utility.convert(node);
+		diskSpace.writeArea(area, myByte);
 	}
 	public void add(Node node){
-		
+		this.node = node;
+		int area = Allocate.allocate();
+		put(area, node);
 	}
 	public void del(Node node){
 		
