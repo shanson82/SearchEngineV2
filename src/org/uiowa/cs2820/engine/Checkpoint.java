eb2c32d74ed5cdd34000db977cef1ad5e72508eb
Netherwill <yicheng-zhao@uiowa.edu>
2014-11-08 14:20:41 -0600

    change to Checkpoint class
    according to Professor Herman's pointers, change checkpoint class

--- src/org/uiowa/cs2820/engine/Checkpoint.java
+++ src/org/uiowa/cs2820/engine/Checkpoint.java
@@ -25,7 +25,14 @@ public class Checkpoint {
 	public void save() throws IOException{
 		FileOutputStream f = new FileOutputStream(filename);
 		byte[] bArray = utility.convert(o);
-		// need to write the byte array, not the object
+		int length = bArray.length;
+		// next encode length into 2 bytes
+		byte [] extra = new byte[2];
+		extra[0] = (byte) (length / 256);
+		extra[1] = (byte) length;
+		// first write the length
+		f.write(extra);
+		// then write normal bytes
 		f.write(bArray);
 		f.close();
 		return;
@@ -33,7 +40,15 @@ public class Checkpoint {
 	
 	public Object restore() throws IOException{
 		FileInputStream f = new FileInputStream(filename);
-		byte[] bArray = new byte[1024];//the length of byte[] may need a bigger value
+		byte [] extra = new byte[2];
+		f.read(extra);
+		int length = (int)extra[0];
+		if (length < 0) length += 256;
+		length = length * 256;
+		int length2 = (int)extra[1];
+		if (length2 < 0) length2 += 256;
+		length = length + length2;
+		byte [] bArray = new byte[length];
 		f.read(bArray);
 		Object obj = utility.revert(bArray);
 		f.close();
