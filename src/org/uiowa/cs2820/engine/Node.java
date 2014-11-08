e6eaa1f116bcb64fae17ee1bb8071b825468fca1
hjshroyer <hugh-shroyer@uiowa.edu>
2014-11-08 00:13:08 -0600

    Update Node.java
    Modified Node class so that second element is an int representing the start value for valueStorage

--- src/org/uiowa/cs2820/engine/Node.java
+++ src/org/uiowa/cs2820/engine/Node.java
@@ -1,25 +1,29 @@
-package src.org.uiowa.cs2820.engine;
+package org.uiowa.cs2820.engine;
 
-import java.util.ArrayList;
+//import java.util.ArrayList;
 
 public class Node {
   // Node is a basic unit in the database
-  byte[] Key;  //Field object converted to byte[]
+  byte[] Key;  // Key of this node for lookup
   //ArrayList<String> Identifiers;
-  long ValueArea; //where ValueStorage area starts
+  int start;
+  int size;    // not yet implemented
   
-  Node(byte[] f, long areaNum) { //(field_object,area_started)
+  Node(byte[] f, int e) {
 	this.Key = f;
-	this.ValueArea = areaNum;
+	this.start = e;
+	//this.Identifiers = new ArrayList<String>();
+	//this.Identifiers.add(id);
     }
   
-  public void add(String id) { 
-	
-    }
+  //public void add(String id) { 
+	//Identifiers.remove(id);  // does nothing if id not already there
+	//Identifiers.add(id);
+    //}
   
-  public void del(String id) {
-	Identifiers.remove(id);
-    }
+  //public void del(String id) {
+	//Identifiers.remove(id);
+    //}
   
   public int getSize() {
 	return size;
