f6e742d5000971ba135d658fd8238f18359f589b
hjshroyer <hugh-shroyer@uiowa.edu>
2014-11-11 10:15:44 -0600

    Update keyStorage.java
    Created del method and modified constructor

--- src/org/uiowa/cs2820/engine/keyStorage.java
+++ src/org/uiowa/cs2820/engine/keyStorage.java
@@ -6,15 +6,18 @@ import src.org.uiowa.cs2820.engine.Allocate;
 import src.org.uiowa.cs2820.engine.Node;
 
 public class keyStorage {
+	protected Diskspace disk;
 	
-	private Node node;
-	
-	int area = 0;
-	keyStorage(Field f){
-		area = Allocate.allocate();
+	public keyStorage(){
+		
+	}
+	public keyStorage(Field f){
+		Node node;
+		int area = Allocate.allocate();
 		int start = Allocate.allocate();
 		byte[] myByte = utility.convert(f);
 		node = Node(myByte, start);
+		add(node);
 	}
 	public boolean init(byte[] key){
 		for (Node n : diskSpace){
@@ -31,28 +34,34 @@ public class keyStorage {
 		
 	}
 	public Node get(int area){
-	//Returns Node located at particular area number	
-		byte[] myByte = Diskspace.readArea(area);
-		Node myNode = utility.revert(myByte);
-		return myNode;
+	//Returns Node located at particular area number
+		try{
+			byte[] myByte = disk.readArea(area);
+			Node myNode = utility.revert(myByte);
+			return myNode;
+		}
+		catch(IOException e){
+			return null;
+		}
+	}
+	public void add(Node node){
+		this.node = node;
+		int area = node.start;
+		put(area, node);
 	}
 	public void put(int area, Node node){
 	//Inserts Node into specified area number of Disk Space	
 		byte[] myByte = utility.convert(node);
-		diskSpace.writeArea(area, myByte);
-	}
-	public void add(Node node){
-		this.node = node;
-		int area = Allocate.allocate();
-		put(area, node);
+		disk.writeArea(area, myByte);
 	}
 	public void del(Node node){
-		
+		Iterator itr = disk.iterator();
+		while(itr.hasNext()){
+			byte[] seq = disk.readArea(itr.next());
+			Node found = utility.revert(seq);
+			if (found == node){
+				Allocate.free(itr.next());
+			}	
+		}
 	}
-	
-}
-	public void del(Node node){
-		
-	}
-	
 }
