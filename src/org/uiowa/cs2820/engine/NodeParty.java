b086a8877cc5d0e6252e9bfadd5a1b18d45fe50c
Will Bulka <willbulka@users.noreply.github.com>
2014-11-12 16:40:47 -0600

    Update NodeParty.java

--- src/org/uiowa/cs2820/engine/NodeParty.java
+++ src/org/uiowa/cs2820/engine/NodeParty.java
@@ -1,4 +1,5 @@
-package src.org.uiowa.cs2820.engine;
+package org.uiowa.cs2820.engine;
+
 ///This is a new class of Nodes made to exist independently of the original node class.
 ///This new NodeParty class is necessary for the construction of the linked list in Value Storage. 
 ///Originally, we thought that ValueStorage would use the Nodes from the Node class, but it is necessary because they hold different values. 
@@ -6,21 +7,30 @@ package src.org.uiowa.cs2820.engine;
 
 public class NodeParty {
 
-	public short area;
-	public byte [] byteguy;
-	public  next;
-	
+	private String id; //could be the areaNum for the firstlocation of the byte
+	private int next; //this should point to next size of byte 
+	private int size; //the size of the byte
+	private int loc; //the soon to be area number 
 
-
-	public NodeParty(short area, byte[] byteguy, short next){
-		this.area = area;
-		this.next = next;
-		this.byteguy = byteguy;
-	}
-	
-	public String toString(){
-		return area + "this is the area";
+	public NodeParty(String guy, int w){
+		this.next = w; //since the list is initially empty the next has no value
+		this.id = guy; 
+		new NodeParty(null, next); //null will become the byte[] from the areaNum							   //while next will find the next loc of next load
+		size = 0; //no byte = no size
+		loc = Allocate.allocate();
+		Diskspace.write(loc,convert(this));
 		
 	}
+	
+	public String getId(){
+		return id; 
+	}
+	
+	public int getNext(){
+		return next;
+	}
+	
+	
 }
-
+	
+	
