2cc41d20c5e09fe13062ed85e3311d7370472486
shanson82 <steven.w.hanson@gmail.com>
2014-11-13 15:35:37 -0600

    creation of LinearFileDatabase

---
+++ src/org/uiowa/cs2820/engine/LinearFileDatabase.java
@@ -0,0 +1,17 @@
+package org.uiowa.cs2820.engine;
+
+public class LinearFileDatabase implements Database {
+	public Node fetch(byte[] key) {
+	
+	}
+	
+	public void store(byte[] key, String id) {
+		return;
+	}
+	
+	
+	// deletion not implemented
+	public void delete(byte[] key, String id) {
+		return;
+	}
+}
