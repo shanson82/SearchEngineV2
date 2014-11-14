f9a255b449fa2092030cddeaef350fbe46af03e8
hjshroyer <hugh-shroyer@uiowa.edu>
2014-11-14 13:13:49 -0600

    created KeyNode.java and refined keyStorage.java

---
+++ src/org/uiowa/cs2820/engine/KeyNode.java
@@ -0,0 +1,35 @@
+package org.uiowa.cs2820.engine;
+
+import java.io.Serializable;
+
+public class KeyNode implements Serializable{
+	private byte[] key;
+	private int value;
+	private int next;
+	
+	KeyNode(byte[] key){
+		this.key = key;
+		this.next = -1;
+		this.value = -1;
+	}
+	
+	public byte[] getKey(){
+		return this.key;
+	}
+	
+	public int getNext(){
+		return this.next;
+	}
+	
+	public void setNext(int area){
+		this.next = area;
+	}
+	
+	public int getValue(){
+		return this.value;
+	}
+	
+	public void setValue(int area){
+		this.value = area;
+	}
+}
