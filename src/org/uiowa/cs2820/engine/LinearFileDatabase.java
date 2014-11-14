3b346b6f2f47d6cf2b21d2ce8ea675273fd9be32
shanson82 <steven.w.hanson@gmail.com>
2014-11-13 19:16:11 -0600

    Will and Steve - implement fetch() method

--- src/org/uiowa/cs2820/engine/LinearFileDatabase.java
+++ src/org/uiowa/cs2820/engine/LinearFileDatabase.java
@@ -1,7 +1,59 @@
 package org.uiowa.cs2820.engine;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
 public class LinearFileDatabase implements Database {
+
+	LinearFileDatabase() {
+		File diskMem = new File("diskSpace.txt");
+		if (diskMem.length() == 0) {
+			try {
+				System.out.println("diskSpace is empty");
+				RandomAccessFile disk = new RandomAccessFile(diskMem, "rw");
+				int writeArea = Allocate.allocate();
+				Head h = new Head(writeArea);				
+				disk.write(utility.convert(h));
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
 	public Node fetch(byte[] key) {
+		// read head of file
+		File diskMem = new File("diskSpace.txt");
+		Diskspace G = new Diskspace(diskMem);
+		byte[] h;
+		try {
+			h = G.readArea(0);
+			Head headNode = (Head) utility.revert(h);
+			if (headNode.getNext() == -1) {
+				return null;
+			}
+			int currentArea = headNode.getNext();
+			while (true) {
+				keyStorage ks = new keyStorage();
+				Node n = ks.get(currentArea);
+				if (n.getKey() == key) {
+					return n;
+				}
+				currentArea = n.getNext();
+				if (currentArea == -1) {
+					return null;
+				}
+				
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	
+		
 	
 	}
 	
