4d1fc912ffab1241a600b452bf84bacfec364dbf
shanson82 <steven.w.hanson@gmail.com>
2014-11-14 14:06:23 -0600

    Will and Steve - changes to FieldSearch

--- src/org/uiowa/cs2820/engine/FieldSearch.java
+++ src/org/uiowa/cs2820/engine/FieldSearch.java
@@ -12,6 +12,15 @@ public class FieldSearch {
 	this.D = d;  
     }
 	
+/*  public String[] findEquals(Field f) {
+	byte[] key = f.toBytes();
+	Node p = D.fetch(key);
+	if (p == null) return new String[0];
+	String[] R = new String[p.Identifiers.size()];
+	R = p.Identifiers.toArray(R);
+	return R;
+    }
+  */
 	public ArrayList<String> findEquals(Field f) {
   		ArrayList<String> identifiers = new ArrayList<String>();
   		byte[] key = f.toBytes();
