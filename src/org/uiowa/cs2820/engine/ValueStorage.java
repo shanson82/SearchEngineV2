2527d2b4558c8d2ae219f834146c1bd10ac88796
shanson82 <steven.w.hanson@gmail.com>
2014-11-14 13:49:28 -0600

    Will and Steve - revise ValueStorage

--- src/org/uiowa/cs2820/engine/ValueStorage.java
+++ src/org/uiowa/cs2820/engine/ValueStorage.java
@@ -6,73 +6,36 @@ import java.util.ArrayList;
 
 public class ValueStorage {
 	
-	private IDNode inode; // identifier node
-	private byte[] key; // key in byte[] format
+	private IDNode idnode; // identifier node
 	private int areaToWrite; // this is non-negative if identifier is the first written to a field
 	private int areaToSearch; // this is non-negative if not the first identifier to be written to a field
 	
-	// constructor to use if the first identifier to be written to a field
-	ValueStorage(String id, int areaToWrite) {
-		byte[] idArray = Utility.convert(id);
-		this.inode = new IDNode(idArray);
-		this.key = null;
-		this.areaToWrite = areaToWrite;
-		this.areaToSearch = -1;
-	}
-	
-	// constructor to use if not the first identifier to be written to a field
-	ValueStorage(String id, int areaToWrite, int areaToSearch) {
-		byte[] idArray = Utility.convert(id);
-		this.inode = new IDNode(idArray);
-		this.key = null;
-		this.areaToWrite = -1;
+	// constructor to use if storing an identifier
+	ValueStorage(String id, int areaToSearch) {
+		System.out.println(id);
+		byte[] idArray = utility.convert(id);
+		this.idnode = new IDNode(idArray);
 		this.areaToSearch = areaToSearch;
-	}
-
-	// constructor to use if searching for identifiers (see FieldSearch)
-	ValueStorage(int areaToSearch) {
-		this.inode = null;
-		this.key = null;
-		this.areaToWrite = -1;
-		this.areaToSearch = areaToSearch;
-	}
-
-	public void store() {
-		byte[] identifier = Utility.convert(this.inode); // identifier node to write to file
-		File diskMem = new File("diskSpace.txt");
-		DiskSpace G = new DiskSpace(diskMem);
+		// can add a check to make sure idArray < 1024 bytes
 		try {
-			// the area in the file to write the identifier is known
-			if (this.areaToWrite != -1) {
-				G.writeArea(this.areaToWrite, identifier);
-				return;
-			} else { // now have to search for place to insert identifier - traverse to the end of the list
-				int searchArea = this.areaToSearch;
-				while (true) {
-					byte[] i = G.readArea(searchArea); 
-					IDNode current = (IDNode) Utility.revert(i);
-					// if getNext == -1, then we are at the end of the list - insert the identifier
-					if (current.getNext() == -1) {
-						// find area to write the identifier
-						int writeArea = Allocate.allocate();
-						// create link/pointer from old end of list to new identifier
-						current.setNext(writeArea);
-						// write current back to file
-						G.writeArea(searchArea, Utility.convert(current));
-						// write new identifier to file
-						G.writeArea(writeArea, Utility.convert(this.inode));
-						return;
-					}
-					// increment to area where next identifier is stored
-					searchArea = current.getNext();
-				}
-			
-			}
+			this.areaToWrite = Allocate.allocate();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		return;
 	}
+	
+	// constructor to use if searching for identifiers (see FieldSearch)
+	ValueStorage(int areaToSearch) {
+		this.idnode = null;
+		this.areaToWrite = -1;
+		this.areaToSearch = areaToSearch;
+	}
+	
+	public int getArea() {
+		return this.areaToWrite;
+	}
+
+
 	
 	public ArrayList<String> load() {
 		ArrayList<String> identifiers = new ArrayList<String>();
@@ -81,28 +44,57 @@ public class ValueStorage {
 			return identifiers;
 		}
 		File diskMem = new File("diskSpace.txt");
-		DiskSpace G = new DiskSpace(diskMem);
+		Diskspace G = new Diskspace(diskMem);
 		int searchArea = this.areaToSearch;
 		while (true) {
 			try {
+				System.out.println(searchArea);
 				byte[] i = G.readArea(searchArea);
-				IDNode current = (IDNode) Utility.revert(i);
+				System.out.println(i);
+				IDNode current = (IDNode) utility.revert(i);
+				System.out.println(current.getNext());
 				// convert identifier from byte[] to String
-				String id = (String) Utility.revert(current.getid());
+				String id = (String) utility.revert(current.getid());
+				System.out.println(id);
 				identifiers.add(id);
 				// if next value == -1, then at end of list of identifiers, return the ArrayList
-				if (current.getNext() == -1) return identifiers;
+				if (current.getNext() == -1) {
+					return identifiers;
+				}
 				// increment to next area to search
 				searchArea = current.getNext();
 			} catch (IOException e) {
 				e.printStackTrace();
-			}
-			
-			
-		}
-	
-		
+			}	
+		}		
 	}
 
+	public void store() {
+		//byte[] identifier = Utility.convert(this.idnode); // identifier node to write to file
+		File diskMem = new File("diskSpace.txt");
+		Diskspace G = new Diskspace(diskMem);
+		try {
+			if (this.areaToSearch == -1) {
+				G.writeArea(this.areaToWrite, utility.convert(this.idnode));
+				return;
+			}
+			int currentArea = this.areaToSearch;
+			while (true) {
+				byte[] currentArray = G.readArea(currentArea);
+				IDNode current = (IDNode) utility.revert(currentArray);
+				if (current.getNext() == -1) {
+					G.writeArea(this.areaToWrite, utility.convert(this.idnode));
+					current.setNext(this.areaToWrite);
+					G.writeArea(currentArea, utility.convert(current));
+					return;
+				}
+				currentArea = current.getNext();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();			
+		}
+		
+	}
+	
+	
 }
-
